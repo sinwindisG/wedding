16667f3982ee0157e6072c9e1df361b4441b8e67
clovermini22 <clovermini22@gmail.com>
2023-06-13 22:48:59 +0900

    Init vue3 project

---
+++ src/App.vue
@@ -0,0 +1,38 @@
+<script setup>
+import Header from './components/Header.vue'
+</script>
+
+<template>
+  <header>
+    <div class="wrapper">
+      <Header msg="🚧공사중" />
+    </div>
+  </header>
+
+  <main>
+  </main>
+</template>
+
+<style scoped>
+header {
+  line-height: 1.5;
+}
+
+@media (min-width: 1024px) {
+  header {
+    display: flex;
+    place-items: center;
+    padding-right: calc(var(--section-gap) / 2);
+  }
+
+  .logo {
+    margin: 0 2rem 0 0;
+  }
+
+  header .wrapper {
+    display: flex;
+    place-items: flex-start;
+    flex-wrap: wrap;
+  }
+}
+</style>
