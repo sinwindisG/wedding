0a25253c9a82a583430577c0f31a66e69f5ce7ce
clovermini22 <clovermini22@gmail.com>
2023-06-17 02:15:45 +0900

    update: footer

--- src/App.vue
+++ src/App.vue
@@ -8,25 +8,42 @@ import Account from "./components/Account.vue";
 </script>
 
 <template>
-  <div class="wrapper">
-    <div class="contents">
-      <div
-        uk-scrollspy="target: > div, h1, img, hr, h3, p, iframe, h4, table, ul; cls: uk-animation-fade; delay: 500"
-      >
-        <Invitation />
-        <Card />
+  <div>
+    <div class="wrapper">
+      <div class="contents">
+        <div
+          uk-scrollspy="target: > div, h1, img, hr, h3, p, iframe, h4, table, ul; cls: uk-animation-fade; delay: 500"
+        >
+          <Invitation />
+          <Card />
+        </div>
+        <Calendar />
+        <Gallery />
+        <Map />
+        <Account />
       </div>
-      <Calendar />
-      <Gallery />
-      <Map />
-      <Account />
     </div>
+    <div class="uk-text-center"><img class="pixel" src="/img/footer.png" /></div>
+    <div class="footer"></div>
   </div>
 </template>
 
 <style scoped>
+.footer {
+  background-color: #bbbbcc;
+  height: 30px;
+  overflow: hidden;
+  text-align: center;
+}
+
+.pixel {
+  image-rendering: pixelated;
+  position: relative;
+  bottom: -24px;
+}
+
 .wrapper {
-  padding: 10px 30px 40px 30px;
+  padding: 10px 30px 10px 30px;
 }
 
 .contents {
