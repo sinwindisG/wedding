67d158dfb1ce64a886d202549b0ff6bfbefc8aa5
clovermini22 <clovermini22@gmail.com>
2023-06-19 23:08:47 +0900

    add: account

--- src/components/Account.vue
+++ src/components/Account.vue
@@ -15,18 +15,26 @@
             <li>
               <a class="uk-accordion-title">신랑측</a>
               <div class="uk-accordion-content">
-                <dl class="uk-description-list">
+                <dl class="uk-description-list uk-description-list-divider">
                   <dt>신랑</dt>
                   <dd>1002-854-632895<br />우리은행 박상현</dd>
+                  <dt>신랑아버지</dt>
+                  <dd>069-07-028625<br />대구은행 박운석</dd>
+                  <dt>신랑어머니</dt>
+                  <dd>704-12-125295<br />농협중앙회 이영화</dd>
                 </dl>
               </div>
             </li>
             <li>
               <a class="uk-accordion-title">신부측</a>
               <div class="uk-accordion-content">
-                <dl class="uk-description-list">
+                <dl class="uk-description-list uk-description-list-divider">
                   <dt>신부</dt>
                   <dd>110-535-223219<br />신한은행 구연재</dd>
+                  <dt>신부아버지</dt>
+                  <dd>707-20-033036<br />SC제일은행 구병문</dd>
+                  <dt>신부어머니</dt>
+                  <dd>125-20-284293<br />SC제일은행 김길순</dd>
                 </dl>
               </div>
             </li>
@@ -49,7 +57,7 @@
 .uk-description-list {
   background-color: #ffffff0c;
   border-radius: 10px;
-  padding: 10px;
+  padding: 20px;
 }
 
 a:hover {
