196a41ae923ebbbf82a76140fd75c552c3ecac71
sinwindisG <sinwindis@gmail.com>
2025-04-02 22:53:33 +0900

    Add files via upload

--- src/components/Account.vue
+++ src/components/Account.vue
@@ -1,7 +1,7 @@
 <template>
   <div
     class="uk-width-1-1"
-    uk-scrollspy="target: > div; cls: uk-animation-fade; delay: 500"
+    uk-scrollspy="target: > div; cls: uk-animation-fade; delay: 220;"
   >
     <div>
       <h3>마음 전하실 곳</h3>
--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -3,7 +3,7 @@ export default {
   methods: {
     sendkakao: function () {
       Kakao.Share.sendCustom({
-        templateId: 116903,
+        templateId: 119187,
       });
     },
   },
