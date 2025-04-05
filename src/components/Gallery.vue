bcbcfd177bea885f773769fe1b849775c297a19c
sinwindisG <sinwindis@gmail.com>
2025-04-06 01:58:30 +0900

    Revert "신규버전을 위해 클린"
    This reverts commit f2170a943618ce1307daca7cd7f643b336d0a8d6.

---
+++ src/components/Gallery.vue
@@ -0,0 +1,55 @@
+<template>
+  <div
+    class="uk-width-1-1"
+    uk-scrollspy="target: > h3, div; cls: uk-animation-fade; delay: 500"
+  >
+    <div>
+      <h3>갤러리</h3>
+      <div
+        class="uk-position-relative uk-visible-toggle uk-light"
+        tabindex="-1"
+        uk-slider="autoplay: true"
+      >
+        <ul class="uk-slider-items uk-child-width-1-1">
+          <li>
+            <img src="/img/gallery/1.jpeg" />
+            <div class="uk-position-center uk-panel"></div>
+          </li>
+          <li>
+            <img src="/img/gallery/2.jpeg" />
+            <div class="uk-position-center uk-panel"></div>
+          </li>
+          <li>
+            <img src="/img/gallery/3.jpeg" />
+            <div class="uk-position-center uk-panel"></div>
+          </li>
+        </ul>
+
+        <a
+          class="uk-position-center-left uk-position-small uk-hidden-hover"
+          href="#"
+          uk-slidenav-previous
+          uk-slider-item="previous"
+        ></a>
+        <a
+          class="uk-position-center-right uk-position-small uk-hidden-hover"
+          href="#"
+          uk-slidenav-next
+          uk-slider-item="next"
+        ></a>
+        <ul class="uk-slider-nav uk-dotnav uk-flex-center uk-margin"></ul>
+      </div>
+    </div>
+    <hr class="uk-divider-icon" />
+  </div>
+</template>
+
+<style scoped>
+.uk-label {
+  font-size: 0.8rem;
+  padding: 0 5px;
+  border-radius: 10px;
+  vertical-align: inherit;
+  margin: 0 5px 0 0;
+}
+</style>
---
+++ src/main.js
@@ -0,0 +1,9 @@
+import './assets/uikit.css'
+import './assets/main.css'
+import './assets/js/uikit.js'
+import './assets/js/uikit-icons.js'
+
+import { createApp } from 'vue'
+import App from './App.vue'
+
+createApp(App).mount('#app')
